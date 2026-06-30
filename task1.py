654158f5a4cda1dcec666f7455433f343a230516
AbbasPeykar <abas.peykar@gmail.com>
2026-06-30 17:05:18 +0330

    Day 1 completed

---
+++ task1.py
@@ -0,0 +1,11 @@
+first_name=input("what is your first name? " )
+
+last_name=input("what is your last name? ")
+
+age=input("how old are you? ")
+
+country=input("what is your country? ")
+
+dream_job=input("what is your dream job? ")
+
+print(f"Hello My name is {first_name} {last_name} and Im {age} years old from {country} and my dream job is {dream_job}. ")
