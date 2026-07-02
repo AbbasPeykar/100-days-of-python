72bc511e6d391ea2e19786ef39a8b0a098379b03
AbbasPeykar <abas.peykar@gmail.com>
2026-07-02 13:42:19 +0330

    Band Name Generator

--- Day1.py
+++ Day1.py
@@ -5,4 +5,7 @@ print("Welcome to the Band Name Generator!")
 city = input("What is the name of the city you grew up in? ")
 pet = input("What is the name of your pet? ")
 
-print(f"Your band name could be: {city} {pet}")
+print(f"Your band nagit add Day1.py
+git commit -m "Add Day 1 - Band Name Generator"
+git pushme could be: {city} {pet}")
+
