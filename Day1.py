4efe364060bdb5e36fef0c51cdfefc9d327f4613
AbbasPeykar <abas.peykar@gmail.com>
2026-07-02 13:34:13 +0330

    Remove Day1 project

--- Day1.py
+++
@@ -1,8 +0,0 @@
-# Band Name Generator
-
-print("Welcome to the Band Name Generator!")
-
-city = input("What is the name of the city you grew up in? ")
-pet = input("What is the name of your pet? ")
-
-print(f"Your band name could be: {city} {pet}")
